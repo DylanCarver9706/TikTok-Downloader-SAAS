6b2143846fb1a29f09caf9cc86d726e02b1de8f4
Dylan Carver <dylancarver14@gmail.com>
2025-03-30 17:06:54 -0500

    Integrate google adsense connection to app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { PostHogProvider } from "./providers/posthog";
 import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
+import Script from "next/script";
 
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
@@ -28,6 +29,13 @@ export default function RootLayout({
   return (
     <Suspense>
       <html lang="en" suppressHydrationWarning>
+        <head>
+          <Script
+            async
+            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_ID}`}
+            crossOrigin="anonymous"
+          />
+        </head>
         <body className={inter.className} suppressHydrationWarning>
           <PostHogProvider>
             <Navbar />
