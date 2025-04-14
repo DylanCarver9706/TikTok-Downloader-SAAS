98dc413c0f9e596f8498cfca7000abf7c09c2c54
Dylan Carver <dylancarver14@gmail.com>
2025-04-13 21:29:27 -0500

    Update s3 bucket env var names to not use reserved aws prefix

--- lib/s3.ts
+++ lib/s3.ts
@@ -8,14 +8,14 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 // Create S3 client
 const s3Client = new S3Client({
-  region: process.env.AWS_REGION,
+  region: process.env.S3_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.S3_ACCESS_KEY!,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
   },
 });
 
-const BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME!;
+const BUCKET_NAME = process.env.S3_BUCKET_NAME!;
 
 // Function to upload file to S3 and get pre-signed URL
 export async function uploadToS3AndGetUrl(
