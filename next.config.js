a45bff81d1f0b68e01d477ced3600800846785ab
Dylan Carver <dylancarver14@gmail.com>
2025-04-13 22:39:36 -0500

    Export environment variables in next.config.js to get around Amplify not reading them

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const nextConfig = {
       bodySizeLimit: "500mb",
     },
   },
+  // Export environment variables to get around Amplify not reading them
+  env: {
+    S3_REGION: process.env.S3_REGION,
+    S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
+    S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
+    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
+  },
 };
 
 module.exports = nextConfig;
