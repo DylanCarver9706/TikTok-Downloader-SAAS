94d37440a849bfb6615725037abde161d2ba83ed
Dylan Carver <dylancarver14@gmail.com>
2026-03-03 23:20:41 -0600

    Fix build errors

--- next.config.js
+++ next.config.js
@@ -5,13 +5,6 @@ const nextConfig = {
       bodySizeLimit: "500mb",
     },
   },
-  // Export environment variables to get around Amplify not reading them
-  env: {
-    S3_REGION: process.env.S3_REGION,
-    S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
-    S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
-    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
-  },
 };
 
 module.exports = nextConfig;
